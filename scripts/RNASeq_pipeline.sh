87a7d38fb56fecdd8395bbd070f96682fe285c02
Sanat Chitnis <sanatchitnis@gmail.com>
2025-01-17 14:07:04 +0000

    Add trimming step with custom adapters

--- scripts/RNASeq_pipeline.sh
+++ scripts/RNASeq_pipeline.sh
@@ -12,3 +12,16 @@ echo "Download complete!"
 fasterq-dump SRR1039509.sra -O /c/Users/sanat/Projects/RNA_Seq_Analysis/data/raw
 
 fastqc SRR1039509_1.fastq SRR1039509_2.fastq -o /c/Users/sanat/Projects/RNA_Seq_Analysis/data/raw 
+
+# Step 3: Trim reads and run FastQC again
+java -jar /c/Softwares/Trimmomatic-0.39/trimmomatic-0.39.jar PE \
+-threads 4 \
+-phred33 \
+SRR1039509_1.fastq SRR1039509_2.fastq \
+SRR1039509_1_paired.fastq SRR1039509_1_unpaired.fastq \
+SRR1039509_2_paired.fastq SRR1039509_2_unpaired.fastq \
+ILLUMINACLIP:custom_adapters.fa:2:30:10 \
+SLIDINGWINDOW:4:20 MINLEN:36
+
+fastqc SRR1039509_1_paired.fastq SRR1039509_2_paired.fastq SRR1039509_1_unpaired.fastq SRR1039509_2_unpaired.fastq -o /c/Users/sanat/Projects/RNA_Seq_Analysis/data/raw
+
