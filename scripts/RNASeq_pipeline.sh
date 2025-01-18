bdd8c2f82b873a59098a96c194e7d05e2af13305
Sanat Chitnis <sanatchitnis@gmail.com>
2025-01-18 14:54:29 +0000

    Add alignment step

--- scripts/RNASeq_pipeline.sh
+++ scripts/RNASeq_pipeline.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # Change to base directory
-cd /c/Users/sanat/Projects/RNA_Seq_Analysis/data/raw
+cd /mnt/c/Users/sanat/Projects/RNA_Seq_Analysis/data/raw
 
 # Step 1: Prefetch SRR files
 echo "Downloading raw data..."
-prefetch --option-file SRR_Acc_List.txt --output-directory /c/Users/sanat/Projects/RNA_Seq_Analysis/data/raw
+prefetch --option-file SRR_Acc_List.txt --output-directory /mnt/c/Users/sanat/Projects/RNA_Seq_Analysis/data/raw
 echo "Download complete!"
 
 # Step 2: Convert to fastq and perform QC
-fasterq-dump SRR1039509.sra -O /c/Users/sanat/Projects/RNA_Seq_Analysis/data/raw
+fasterq-dump SRR1039509.sra -O /mnt/c/Users/sanat/Projects/RNA_Seq_Analysis/data/raw
 
-fastqc SRR1039509_1.fastq SRR1039509_2.fastq -o /c/Users/sanat/Projects/RNA_Seq_Analysis/data/raw 
+fastqc SRR1039509_1.fastq SRR1039509_2.fastq -o /mnt/c/Users/sanat/Projects/RNA_Seq_Analysis/data/raw 
 
 # Step 3: Trim reads and run FastQC again
-java -jar /c/Softwares/Trimmomatic-0.39/trimmomatic-0.39.jar PE \
+java -jar /mnt/c/Softwares/Trimmomatic-0.39/trimmomatic-0.39.jar PE \
 -threads 4 \
 -phred33 \
 SRR1039509_1.fastq SRR1039509_2.fastq \
@@ -23,5 +23,13 @@ SRR1039509_2_paired.fastq SRR1039509_2_unpaired.fastq \
 ILLUMINACLIP:custom_adapters.fa:2:30:10 \
 SLIDINGWINDOW:4:20 MINLEN:36
 
-fastqc SRR1039509_1_paired.fastq SRR1039509_2_paired.fastq SRR1039509_1_unpaired.fastq SRR1039509_2_unpaired.fastq -o /c/Users/sanat/Projects/RNA_Seq_Analysis/data/raw
+fastqc SRR1039509_1_paired.fastq SRR1039509_2_paired.fastq SRR1039509_1_unpaired.fastq SRR1039509_2_unpaired.fastq -o /mnt/c/Users/sanat/Projects/RNA_Seq_Analysis/data/raw
 
+# Step 4: Alignment with HISAT2 and Convert SAM to BAM
+# Alignment
+hisat2 -x /mnt/c/Users/sanat/Projects/RNA_Seq_Analysis/data/raw/grch38_genome/genome -1 SRR1039509_1_paired.fastq -2 SRR1039509_2_paired.fastq -S aligned_reads.sam
+
+# SAM to BAM
+samtools view -bS aligned_reads.sam > aligned_reads.bam
+samtools sort aligned_reads.bam -o aligned_reads_sorted.bam
+samtools index aligned_reads_sorted.bam
